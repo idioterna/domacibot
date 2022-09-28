ae3f412ac36aa4987e21214d497ef7457b637cc9
Jure Koren <jure@hehe.si>
2022-09-29 00:15:44 +0200

    add temperatures, upload radar image, nag on birthdays

--- bot.py
+++ bot.py
@@ -1,54 +1,82 @@
-import discord, time
+import discord, time, urllib, random
+import settings
 from pyquery import PyQuery as pq
+from io import BytesIO
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
+intents.presences = True
 
 client = discord.Client(intents=intents)
 
+birthdays_done = {}
 cache = {}
 
-def cached():
+def l2u(s):
+    return str(bytes(s, 'latin1'), 'utf-8')
+
+def cached(what, url):
     global cache
-    if not cache.get('data') or cache.get('age', 0) + 3600 < time.time():
+    if not cache.get(f'{what}_data') or cache.get(f'{what}_age', 0) + 300 < time.time():
         try:
-            newdata = pq(url="https://meteo.arso.gov.si/uploads/probase/www/fproduct/text/sl/fcast_si_text.html")
-            cache['data'] = newdata
+            newdata = pq(url=url)
+            cache[f'{what}_data'] = newdata
         except:
             pass
-        cache['age'] = time.time()
-    return cache['data']
+        cache[f'{what}_age'] = time.time()
+    return cache[f'{what}_data']
 
-def getvreme(what='short'): # or long or full
-    d = cached()
+def getvreme(what='long'): # or long or full
+    napoved = cached('napoved', 'https://meteo.arso.gov.si/uploads/probase/www/fproduct/text/sl/fcast_si_text.html')
+    podatki = cached('podatki', 'https://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/sl/observation_si_latest.html')
+    text_podatki = '\n'
+    for location in settings.LOCATIONS:
+        line = podatki('td').filter(lambda i, this: location == l2u(pq(this).text()))
+        vreme_text = l2u(line.next().next()[0].text)
+        if not vreme_text: vreme_text = 'neznano'
+        stopinje_text = l2u(line.next().next().next()[0].text)
+        if not stopinje_text: stopinje_text = '??'
+        text_podatki += location.split()[-1] + ', ' + vreme_text + ', ' + stopinje_text + '°C\n'
+    text_vreme = ''
     if what == 'long':
-        long = ''
-        p = d("h2").next().next().next()
+        p = napoved("h2").next().next().next()
         while True:
             el = p[0]
             if el.tag != 'p':
                 break
-            long += '\n' + str(bytes(el.text, 'latin1'), 'utf-8')
+            text_vreme += '\n' + l2u(el.text)
             p = p.next()
-        return long
     elif what == 'full':
-        full = ''
-        p = d("h2:first")
+        text_vreme = ''
+        p = napoved("h2:first")
         while True:
             el = p[0]
             if el.tag not in ('h2', 'p'):
                 break
-            full += '\n' + str(bytes(el.text, 'latin1'), 'utf-8')
+            text_vreme += '\n' + l2u(el.text)
             p = p.next()
-        return full
     elif what == 'short':
-        return str(bytes(d("p:first")[0].text, 'latin1'), 'utf-8')
+        text_vreme = l2u(napoved("p:first")[0].text)
     else:
-        return 'ne vem kaj je to ' + what
+        return 'ne vem kaj je to ' + what + ', lahko je short, long, full'
+    return text_podatki + text_vreme
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_presence_update(before, after):
+    print(f'{after.display_name} changed presence to {after.raw_status}')
+    today = time.strftime('%d-%m')
+    year = int(time.strftime('%Y'))
+    birthday = settings.BIRTHDAYS.get(after.display_name)
+    if birthday == today and birthdays_done.get(after.display_name, 0) < year:
+        channel = client.get_channel(895560470210150440)
+        birthdays_done[after.display_name] = year
+        message = random.choice(settings.BIRTHDAY_MESSAGES)
+        await channel.send(f'Zdravo {after.display_name}!\n{message}')
 
 @client.event
 async def on_message(message):
@@ -59,11 +87,12 @@ async def on_message(message):
         try:
             what = message.content.split()[1]
         except IndexError:
-            what = 'short'
+            what = 'long'
         await message.channel.send('```' + getvreme(what) + '```')
 
     if message.content.lower().startswith('radar') and len(message.content.split()) < 2:
-        await message.channel.send('https://meteo.arso.gov.si/uploads/probase/www/observ/radar/si0-rm-anim.gif')
+        radar_gif = BytesIO(urllib.request.urlopen('https://meteo.arso.gov.si/uploads/probase/www/observ/radar/si0-rm-anim.gif').read())
+        await message.channel.send(file=discord.File(radar_gif, filename='radar.gif'))
 
 client.run(settings.TOKEN)
 
--- settings.py
+++
@@ -1,3 +0,0 @@
-
-TOKEN = 'discord bot token'
-
